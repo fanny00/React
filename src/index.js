d7c3b83128bec352643bd5e0ec191e139ccdc946
Fanny Campos <fanny.campos@amsoft.cl>
2025-01-06 12:09:22 -0300

    objetos literales

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 
+const persona = {
+  nombre: 'Tony',
+  apellido: 'Stark',
+  edad: 45,
+  direccion: {
+    ciudad: 'New York',
+    zip: 553581,
+    lat: 14.3232,
+    lng: 34.98656
+  }
+};
+
+console.log ( persona );
+
+console.table ( persona );
+
+console.log( {
+    //persona:persona
+    persona
+} );
+
+// Asingación mal hecha, copia de referencia
+/*const persona2 = persona;
+persona2.nombre = 'Peter';
+
+console.log( persona );
+console.log( persona2 );*/
+
+
+// Clon del Objeto
+const persona2 = { ...persona };
+persona2.nombre = 'Peter';
+
+console.log( persona );
+console.log( persona2 );
