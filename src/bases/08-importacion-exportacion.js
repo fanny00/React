6f7c9073f8ab0214365139295655925f238c046c
Fanny Campos <fanny.campos@amsoft.cl>
2025-01-10 12:32:54 -0300

    Promesas

--- src/bases/08-importacion-exportacion.js
+++ src/bases/08-importacion-exportacion.js
@@ -6,21 +6,24 @@
 // import { owners } from "./data/heroes2";
 // import  heroes, { owners } from './data/heroes2';
 // import { heroes, owners } from "./data/heroes2";
-import  heroes, { owners } from '../data/heroes2';
+// import  heroes, { owners } from '../data/heroes2';
+import  heroes from '../data/heroes2';
 
  // console.log( heroes );
 
- const getHeroeById = (id) => heroes.find( ( heroe ) => heroe.id === id );
+ export const getHeroeById = (id) => heroes.find( ( heroe ) => heroe.id === id );
   //{
    // return {};
    // return heroes.find( ( heroe ) => heroe.id === id );
  // }
 
- console.log( getHeroeById(2) );
+ // console.log( getHeroeById(2) );
 
- const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
+ export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
 
- console.log( getHeroesByOwner('DC'));
- console.log( getHeroesByOwner('Marvel'));
+//  console.log( getHeroesByOwner('DC'));
+//  console.log( getHeroesByOwner('Marvel'));
 
- console.log( owners );
+//  console.log( owners );
+
+ 
